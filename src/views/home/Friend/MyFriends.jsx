f9323756d2b175bc2095f9cc1a4714e1578ee377
KarmaAndEmptiness <wupeiling@tutamail.com>
2024-10-25 13:59:53 +0000

    2024.10.25 update

--- src/views/home/Friend/MyFriends.jsx
+++ src/views/home/Friend/MyFriends.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Tabs, Tab, Box, List, ListItemButton, ListItemAvatar, Avatar, ListItemText, Badge, Divider } from '@mui/material'
+import { Tabs, Tab, Box, List, ListItemButton, ListItemAvatar, Avatar, ListItemText, Badge, Divider, Tooltip, IconButton, Menu, MenuItem, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, TextField, Popover } from '@mui/material'
+import { Chat as ChatIcon, MoreHoriz as MoreHorizIcon, Close as CloseIcon } from '@mui/icons-material';
 import { styled } from '@mui/material/styles'
 
 import { friends } from '~/data/friends.js'
@@ -15,40 +16,87 @@ const tabsLi = [
 export default function MyFriends() {
     const [value, setValue] = React.useState(0);
     const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [friendInfoDialogOpen, setfriendInfoDialogOpen] = React.useState(false);
+    const [editCommentDialogOpen, setEditCommentDialogOpen] = React.useState(false);
+    const [confirmDeleteFriendDialog, setConfirmDeleteFriendDialog] = React.useState(false);
+    const open = Boolean(anchorEl);
+
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+    const handleSelectMenu = (event, id) => {
+        event.stopPropagation();
+        switch (id) {
+            case 0:
+                setEditCommentDialogOpen(true);
+                break;
+            case 1:
+                setConfirmDeleteFriendDialog(true);
+                break;
+        }
+    };
+    const handleSelectMenuClose = (event) => {
+        event.stopPropagation();
+        setAnchorEl(null);
+    };
     const handleListItemClick = (event, index) => {
         setSelectedIndex(index);
+        setfriendInfoDialogOpen(true);
     };
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
+    const handlefriendInfoDialogClose = () => {
+        setfriendInfoDialogOpen(false);
+    }
+
+    const handleSendMsgIconClick = (event) => {
+        event.stopPropagation();
+    }
+    const handleConfirmDeleteFriendDialogClose = (event) => {
+        event.stopPropagation();
+        setConfirmDeleteFriendDialog(false);
+    }
+    const handleEditCommentDialogClose = (event) => {
+        event.stopPropagation();
+        setEditCommentDialogOpen(false);
+    }
     return (
         <>
+            {/* 列表顶部的分类导航栏 */}
             <Box sx={{ width: '100%' }}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider', paddingLeft: '1rem' }}>
                     <Tabs value={value} onChange={handleChange} aria-label="tabs">
                         {
                             tabsLi.map((tab, idx) => (
-                                <Tab label={tab.label} />
+                                <Tab key={tab.label} label={tab.label} />
                             ))
                         }
                     </Tabs>
                 </Box>
             </Box>
+
+            {/* 好友列表 */}
             <List
                 sx={
                     {
                         overflow: 'auto',
-                        maxHeight: '100%',
+                        maxHeight: '111%',
                         scrollbarWidth: 'thin',
                     }
                 }
             >
                 {
                     friends && friends.map((friend, index) => (
-                        <React.Fragment key={`react-fragment-${friend.username}`}>
+                        <React.Fragment key={`friends-${friend.username}-${friend.id}`}>
                             <ListItemButton
-                                selected={selectedIndex === index}
+                                // selected={selectedIndex === index}
                                 onClick={(event) => handleListItemClick(event, index)}
                             >
                                 <ListItemAvatar>
@@ -87,14 +135,43 @@ export default function MyFriends() {
                                             }
                                         }
                                     >
-                                        {friend.comment}
+                                        个性签名：{friend.personalSign === null || friend.personalSign === ' ' ? '暂无' : friend.personalSign}
                                     </span>} />
                                 <ListItemText secondary={<span style={
                                     {
                                         fontSize: '12px',
                                     }
                                 }>
-                                    {friend.phone}
+                                    <Tooltip title="发消息">
+                                        <IconButton onClick={handleSendMsgIconClick}>
+                                            <ChatIcon />
+                                        </IconButton>
+                                    </Tooltip>
+
+                                    <IconButton
+                                        onMouseEnter={handlePopoverOpen}
+                                        onMouseLeave={handlePopoverClose}
+                                    >
+                                        <MoreHorizIcon />
+                                    </IconButton>
+
+                                    <Popover
+                                        anchorEl={anchorEl}
+                                        open={open}
+                                        onClose={handleSelectMenuClose}
+                                        anchorOrigin={{
+                                            vertical: 'center',
+                                            horizontal: 'left',
+                                        }}
+                                        transformOrigin={{
+                                            vertical: 'center',
+                                            horizontal: 'right',
+                                        }}
+                                        elevation={1}
+                                    >
+                                        <MenuItem elevation={1} onClick={(event) => handleSelectMenu(event, 0)}>编辑备注</MenuItem>
+                                        <MenuItem elevation={1} onClick={(event) => handleSelectMenu(event, 1)}>删除</MenuItem>
+                                    </Popover>
                                 </span>}
                                     sx={
                                         {
@@ -110,6 +187,151 @@ export default function MyFriends() {
                     ))
                 }
             </List>
+
+            {/* 好友详细信息模态框 */}
+            <Dialog
+                open={friendInfoDialogOpen}
+                onClose={handlefriendInfoDialogClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <IconButton
+                    aria-label="close"
+                    onClick={handlefriendInfoDialogClose}
+                    sx={(theme) => ({
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: theme.palette.grey[500],
+                        width: '20px',
+                        height: '20px'
+                    })}
+                >
+                    <CloseIcon sx={
+                        {
+                            fontSize: '20px',
+                            color: '#fff'
+                        }
+                    } />
+                </IconButton>
+                <DialogContent sx={
+                    {
+                        width: 360, height: 460,
+                        padding: 0,
+                        background: 'skyblue'
+                    }
+                }>
+                    <Box sx={
+                        {
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexWrap: 'wrap',
+                            paddingTop: 10
+                        }
+                    }>
+                        <Avatar src={avatar} sx={
+                            {
+                                width: 50, height: 50
+                            }
+                        } />
+                        <Box sx={
+                            {
+                                width: '100%',
+                                fontSize: 20,
+                                fontWeight: 700,
+                                textAlign: 'center'
+                            }
+                        }>hola world</Box>
+                    </Box>
+
+                    <Box sx={
+                        {
+                            background: '#fff',
+                            padding: '5px 10px',
+                            height: 'calc(100% - 160px)'
+                        }
+                    }>
+                        <Box>个性签名：hhhhh</Box>
+                        <Box>手机：12345645</Box>
+                        <Box>用户名：0v0</Box>
+                        <Box>性别：男</Box>
+                        <Box>邮箱：男</Box>
+                        <Box>分组：全部</Box>
+                        <Box>备注：1</Box>
+                    </Box>
+                </DialogContent>
+                <DialogActions
+                    sx={
+                        {
+                            display: 'flex',
+                            justifyContent: 'center'
+                        }
+                    }
+                >
+                    <Button variant='contained' onClick={handlefriendInfoDialogClose} sx={
+                        {
+                            width: '60%'
+                        }
+                    }>
+                        发送消息
+                    </Button>
+                </DialogActions>
+            </Dialog >
+
+            {/* 编辑备注模态框 */}
+            <Dialog
+                open={editCommentDialogOpen}
+                onClose={handleEditCommentDialogClose}
+                PaperProps={{
+                    component: 'form',
+                    // onSubmit: (event) => {
+                    //     event.preventDefault();
+                    //     const formData = new FormData(event.currentTarget);
+                    //     const formJson = Object.fromEntries(formData.entries());
+                    //     const email = formJson.email;
+                    //     console.log(email);
+                    //     handleClose();
+                    // },
+                }}
+            >
+                <DialogTitle>修改备注</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        name="email"
+                        label="好友备注"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleEditCommentDialogClose}>取消</Button>
+                    <Button onClick={handleEditCommentDialogClose}>确定</Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* 删除好友确认模态框 */}
+            <Dialog
+                open={confirmDeleteFriendDialog}
+                keepMounted
+                onClose={handleConfirmDeleteFriendDialogClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        Let Google help apps determine location. This means sending anonymous
+                        location data to Google, even when no apps are running.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleConfirmDeleteFriendDialogClose}>取消</Button>
+                    <Button onClick={handleConfirmDeleteFriendDialogClose}>确认</Button>
+                </DialogActions>
+            </Dialog>
         </>
 
     )
--- src/views/home/Friend/MyGroup.jsx
+++ src/views/home/Friend/MyGroup.jsx
@@ -1,9 +1,267 @@
 import React from 'react'
+import { Avatar, Box, Button, Divider, Drawer, List, Paper, Popover, Stack, Tab, Tabs, TextField, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles';
+
+import { groups } from '~/data/groups.js'
+import avatar from '~/assets/img/bg.jpg'
+
+const tabsLi = [
+    {
+        name: '全部群聊'
+    },
+    {
+        name: '我创建的'
+    },
+    {
+        name: '我加入的'
+    },
+
+]
+
 export default function MyGroup() {
+    const [activeGroup, setActiveGroup] = React.useState(0);
+    const [groupInfoDrawerOpen, setGroupInfoDrawerOpen] = React.useState(true);
+    const [businessCardOpenAnchorEl, setBusinessCardOpenAnchorEl] = React.useState(null);
+
+    const editBusinessCardOpen = Boolean(businessCardOpenAnchorEl);
+    const handleChangeGroup = (event, selectedGroup) => {
+        setActiveGroup(selectedGroup)
+    }
+    const handleGroupInfoDrawerClose = () => {
+        setGroupInfoDrawerOpen(false);
+    }
+
+    const handleGroupPaperClick = (index) => {
+        setGroupInfoDrawerOpen(true);
+    }
+
+    const handleEditGroupBusinessCardOpen = (event) => {
+        setBusinessCardOpenAnchorEl(event.currentTarget);
+    }
+    const handleEditGroupBusinessCardClose = () => {
+        setBusinessCardOpenAnchorEl(null);
+    }
     return (
         <>
+            {/* 列表顶部的分类导航栏 */}
+            <Box sx={{ width: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider', paddingLeft: '1rem' }}>
+                    <Tabs value={activeGroup} onChange={handleChangeGroup} aria-label="tabs">
+                        {
+                            tabsLi.map((tab, idx) => (
+                                <Tab key={tab.name} label={tab.name} />
+                            ))
+                        }
+                    </Tabs>
+                </Box>
+            </Box>
 
+            {/* 群聊列表 */}
+            <List
+                sx={
+                    {
+                        overflow: 'auto',
+                        maxHeight: '111%',
+                        scrollbarWidth: 'thin',
+                        padding: '10px 10px'
+                    }
+                }
+            >
+                <Stack direction="row" spacing={2}>
+                    {
+                        groups.map((group, idx) => (
+                            <GroupPaper key={group.groupName} variant='outlined' square={false}
+                                onClick={
+                                    (event) => handleGroupPaperClick(idx)
+                                }
+                                sx={
+                                    {
+                                        display: 'flex',
+                                        alignItems: 'center'
+                                    }
+                                }>
+                                <Avatar src={avatar} alt={group.groupName} sx={
+                                    {
+                                        background: 'pink'
+                                    }
+                                } />
+                                <Box sx={
+                                    {
+                                        marginLeft: 1
+                                    }
+                                }>
+                                    {group.groupName}
+                                </Box>
+                            </GroupPaper>
+                        ))
+                    }
+                </Stack>
+            </List>
+
+            {/* 群信息drawer */}
+            <Drawer
+                anchor='right'
+                open={groupInfoDrawerOpen}
+                onClose={handleGroupInfoDrawerClose}
+                sx={
+                    {
+                        zIndex: 1202,
+                        alignItems: 'center'
+                    }
+                }
+            >
+                <Box
+                    sx={
+                        {
+                            width: 400,
+                        }
+                    }
+                >
+                    <Box
+                        sx={
+                            {
+                                textAlign: 'center',
+                                padding: '20px 0'
+                            }
+                        }
+                    >
+                        群信息
+                    </Box>
+                </Box>
+                <Divider />
+
+                <Stack
+                    sx={
+                        {
+                            padding: '10px 10px'
+                        }
+                    }
+                    spacing={1}
+                >
+                    <Box>
+                        <Box>群名称</Box>
+                        <span style={
+                            {
+                                color: 'gray',
+                                fontSize: 14
+                            }
+                        }>234</span>
+                    </Box>
+                    <Box>
+                        <Box>群简介</Box>
+                        <span style={
+                            {
+                                color: 'gray',
+                                fontSize: 14
+                            }
+                        }>暂无群简介</span>
+                    </Box>
+                    <Box>
+                        <Box
+                            sx={
+                                {
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    padding: '0 5px 0 0px'
+                                }
+                            }
+                        >
+                            <Box>群名片</Box>
+                            <Button onClick={handleEditGroupBusinessCardOpen} variant='outlined'>
+                                设置
+                            </Button>
+
+                            <Popover
+                                open={editBusinessCardOpen}
+                                anchorEl={businessCardOpenAnchorEl}
+                                onClose={handleEditGroupBusinessCardClose}
+                                anchorOrigin={{
+                                    vertical: 'center',
+                                    horizontal: 'left',
+                                }}
+                                transformOrigin={
+                                    {
+                                        vertical: 'center',
+                                        horizontal: 'right'
+                                    }
+                                }
+                            >
+                                <Box sx={{ fontSize: 15 }}>设置我的群名片</Box>
+                                <Divider />
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="groupBussinessCard"
+                                    name="groupBussinessCard"
+                                    label="群名片"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                />
+                                <Button variant='contained'>确定</Button>
+                            </Popover>
+                        </Box>
+                        <span style={
+                            {
+                                color: 'gray',
+                                fontSize: 14
+                            }
+                        }>234</span>
+                    </Box>
+                    <Box>
+                        <Box
+                            sx={
+                                {
+                                    display: 'flex',
+                                    justifyContent: 'space-between',
+                                    padding: '0 5px 0 0'
+                                }
+                            }
+                        >
+                            <Box>群成员</Box>
+                            <Box>
+                                3人
+                            </Box>
+                        </Box>
+                        <span style={
+                            {
+                                color: 'gray',
+                                fontSize: 14
+                            }
+                        }>234</span>
+                    </Box>
+                    <Box>
+                        <Box>群公告</Box>
+                        <span style={
+                            {
+                                color: 'gray',
+                                fontSize: 14
+                            }
+                        }>wewe</span>
+                    </Box>
+                </Stack>
+
+                <Button variant="contained"
+                    sx={
+                        {
+                            width: '80%',
+                            margin: 'auto 10% 20px 10%',
+                        }
+                    }
+                >群聊管理</Button>
+            </Drawer>
         </>
     )
 }
 
+const GroupPaper = styled(Paper)(({ theme }) => ({
+    width: 260,
+    height: 100,
+    padding: theme.spacing(2),
+    ...theme.typography.body2,
+    textAlign: 'center',
+    cursor: 'pointer',
+    '&:hover': {
+        border: '1px solid #1976d2'
+    }
+}));
